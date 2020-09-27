85c6c340e5ece84545117542bd2af91ef0c4b35d
Brullrr <rbrull@yahoo.com>
2020-09-27 18:04:42 +0900

    Added Library Project

---
+++ src/containers/App.js
@@ -0,0 +1,22 @@
+import React, { Component, Fragment } from 'react';
+import Library from '../Library/Library'
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
+  
+  
+  render() {
+    return (
+      <Fragment>
+        <Library />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
