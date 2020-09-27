dd9d580286c034b3de6782535dd23bcb05bb7a63
Brullrr <rbrull@yahoo.com>
2020-09-27 18:18:31 +0900

    Updates

---
+++ precache-manifest.1a48a1edad6eb1df364279bc9c7c9d5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da72d00111e793923660e6766d5820e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbee2ff681e8e5c21900",
+    "url": "/static/css/main.2d0a1551.chunk.css"
+  },
+  {
+    "revision": "c6f1e0880f588c0d7a2d",
+    "url": "/static/js/2.f425dac4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f425dac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbee2ff681e8e5c21900",
+    "url": "/static/js/main.b1030191.chunk.js"
+  },
+  {
+    "revision": "bd992763667b595be7da",
+    "url": "/static/js/runtime-main.76fafacd.js"
+  },
+  {
+    "revision": "13ec8e586cd46581cdad63f99af56865",
+    "url": "/static/media/book_cover_kawa.13ec8e58.png"
+  }
+]);
